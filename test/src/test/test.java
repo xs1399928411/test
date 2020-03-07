0f766b806fbc27a051d09755f9feae71acfe8b68
Administrator <Administrator@xs>
2020-03-07 15:55:07 +0800

    1

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -1,12 +1,7 @@
 package test;
 
 public class test {
-public static void main(String[] args) {
-	System.out.println("123");
-	
-
-
-
-
-}
+	public static void main(String[] args) {
+		System.out.println("15634653463");
+	}
 }
